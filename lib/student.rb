db9790f2733d4afa464c95350dc1c033632b5786
Laura Felce <crepu83@yahoo.it>
2019-02-20 15:07:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,12 +28,6 @@ attr_reader :id
   end
 
 
-  def update
-    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
-    DB[:conn].execute(sql, self.name, self.grade, self.id)
-  end
-
-
   def save
     if self.id
     self.update
@@ -46,6 +40,13 @@ attr_reader :id
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
   end
+
+
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+
 
 
 
