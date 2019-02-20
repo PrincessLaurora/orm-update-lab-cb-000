fe8628955d1c4936dde16a7e8ae2e95f52dd1669
Laura Felce <crepu83@yahoo.it>
2019-02-20 15:05:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,7 +37,7 @@ attr_reader :id
   def save
     if self.id
     self.update
-  else
+    else
     sql = <<-SQL
       INSERT INTO students (name, grade)
       VALUES (?, ?)
@@ -45,7 +45,7 @@ attr_reader :id
     DB[:conn].execute(sql, self.name, self.grade)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
-  end  
+  end
 
 
 
